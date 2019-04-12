2c216cd0f90d08a6d154307038acf2e8f6fe5e0b
Dominic DiPuma <dominic.dipuma@rutgers.edu>
2019-04-12 19:23:04 -0400

    Use randomly generated int array from 0 to 255 for timing tests Plot all data on a single log-log axis for benchmarking Next step is to iterate this (generate many random arrays of a given size, time all algorithms, and produce average and error)

--- timing_experiments.py
+++ timing_experiments.py
@@ -1,22 +1,28 @@
-from haar import *
-import matplotlib.pyplot as plt
-import timeit
 import functools
-from random import shuffle
+import timeit
+
+import matplotlib.pyplot as plt
+
+from haar import *
 
 
 def one_dim_forward_benchmark():
     ordered_timings = []
     inplace_timings = []
     matrix_timings = []
-    data_sizes = [1024, 2048, 4096, 8192]
+
+    data_sizes = [2**N for N in range(5, 13)]
+
     for data_size in data_sizes:
-        data_array = list(range(data_size))
-        ordered_timer = timeit.Timer(functools.partial(ordered_fast_1d_haar_transform, data_array[:]))
+        data_array = np.random.randint(256, size=data_size)
+
+        ordered_timer = timeit.Timer(functools.partial(ordered_fast_1d_haar_transform, data_array))
         ordered_timings.append(ordered_timer.timeit(3))
-        inplace_timer = timeit.Timer(functools.partial(inplace_fast_1d_haar_transform, data_array[:]))
+
+        inplace_timer = timeit.Timer(functools.partial(inplace_fast_1d_haar_transform, data_array))
         inplace_timings.append(inplace_timer.timeit(3))
-        matrix_timer = timeit.Timer(functools.partial(matrix_1d_haar_transform, data_array[:]))
+
+        matrix_timer = timeit.Timer(functools.partial(matrix_1d_haar_transform, data_array))
         matrix_timings.append(matrix_timer.timeit(3))
 
     columns = ('Ordered Haar Transform Runtime', 'In-Place Haar Transform Runtime', 'Matrix Haar Transform Runtime')
@@ -29,25 +35,17 @@ def one_dim_forward_benchmark():
     plt.suptitle("1D Haar Transform Algorithm Runtimes")
     fig.subplots_adjust(left=0.2, top=0.8, wspace=1)
 
-    ax = plt.subplot2grid((2, 3), (1, 0), colspan=4, rowspan=2)
+    plt.subplot(211)
+    plt.plot(np.log2(data_sizes), np.log2(ordered_timings), "b-", label="ordered (fast)")
+    plt.plot(np.log2(data_sizes), np.log2(inplace_timings), "r-", label="inplace (fast)")
+    plt.plot(np.log2(data_sizes), np.log2(matrix_timings), "k-", label="matrix (slow)")
+    plt.xlabel("$log_2$ size (Length of Input Array)")
+    plt.ylabel("$log_2$ time (s)")
+    plt.legend()
+
+    ax = plt.subplot(212)
     ax.table(cellText=cell_text, rowLabels=rows, colLabels=columns, loc='upper center')
     ax.axis("off")
-
-    plt.subplot2grid((2, 3), (0, 0))
-    plt.plot(data_sizes, ordered_timings)
-    plt.title("Ordered Haar Transform", y=1.08)
-    plt.xlabel("Input Size (Length of Array)")
-    plt.ylabel("Input Length")
-    plt.subplot2grid((2, 3), (0, 1))
-    plt.plot(data_sizes, inplace_timings)
-    plt.title("In-Place Haar Transform", y=1.08)
-    plt.xlabel("Input Size (Length of Array)")
-    plt.ylabel("Input Length")
-    plt.subplot2grid((2, 3), (0, 2))
-    plt.plot(data_sizes, matrix_timings)
-    plt.title("Matrix Haar Transform", y=1.08)
-    plt.xlabel("Input Size (Length of Array)")
-    plt.ylabel("Input Length")
 
     fig.set_size_inches(w=12, h=10)
     plt.show()
