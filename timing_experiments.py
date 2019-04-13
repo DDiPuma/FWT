89812cbf37200a4e1da2ab9f043f93ec565f07d8
Brendan <abae97@gmail.com>
2019-04-12 22:54:32 -0400

    made some stuff

--- timing_experiments.py
+++ timing_experiments.py
@@ -81,5 +81,67 @@ def one_dim_forward_benchmark():
     plt.show()
 
 
+def fast_one_dim_forward_benchmark():
+    ordered_time_averages = []
+    inplace_time_averages = []
+
+    ordered_time_mins = []
+    inplace_time_mins = []
+
+    ordered_time_maxes = []
+    inplace_time_maxes = []
+
+    ordered_time_std_devs = []
+    inplace_time_std_devs = []
+
+    data_sizes = [2**N for N in range(1, 20)]
+
+    for data_size in data_sizes:
+        ordered_timings = []
+        inplace_timings = []
+
+        for trial in range(25):
+            data_array = np.random.randint(256, size=data_size)
+
+            ordered_timer = timeit.Timer(functools.partial(ordered_fast_1d_haar_transform, data_array))
+            ordered_timings.append(ordered_timer.timeit(3))
+
+            inplace_timer = timeit.Timer(functools.partial(inplace_fast_1d_haar_transform, data_array))
+            inplace_timings.append(inplace_timer.timeit(3))
+
+        ordered_time_averages.append(np.mean(ordered_timings))
+        inplace_time_averages.append(np.mean(inplace_timings))
+        ordered_time_maxes.append(max(ordered_timings))
+        inplace_time_maxes.append(max(inplace_timings))
+        ordered_time_mins.append(min(ordered_timings))
+        inplace_time_mins.append(min(inplace_timings))
+        ordered_time_std_devs.append(np.std(np.log2(ordered_timings)))
+        inplace_time_std_devs.append(np.std(np.log2(inplace_timings)))
+
+    columns = ('Ordered Haar Transform Runtime', 'In-Place Haar Transform Runtime')
+    rows = ["Input Length = {}".format(x) for x in data_sizes]
+    cell_text = []
+    for time_tuple in zip(ordered_time_averages, inplace_time_averages):
+        cell_text.append(["{} seconds".format(time_data) for time_data in time_tuple])
+
+    fig = plt.figure(1)
+    plt.suptitle("1D Haar Transform Algorithm Runtimes")
+    fig.subplots_adjust(left=0.2, top=0.8, wspace=1)
+
+    plt.subplot(211)
+    plt.errorbar(np.log2(data_sizes), np.log2(ordered_time_averages), yerr=ordered_time_std_devs, fmt="b-", label="ordered (fast)")
+    plt.errorbar(np.log2(data_sizes), np.log2(inplace_time_averages), yerr=inplace_time_std_devs, fmt="r-", label="inplace (fast)")
+    plt.xlabel("$log_2$ size (Length of Input Array)")
+    plt.ylabel("$log_2$ time (s)")
+    plt.legend()
+
+    ax = plt.subplot(212)
+    ax.table(cellText=cell_text, rowLabels=rows, colLabels=columns, loc='upper center')
+    ax.axis("off")
+
+    fig.set_size_inches(w=12, h=10)
+    plt.show()
+
+
 if __name__ == '__main__':
-    one_dim_forward_benchmark()
+    fast_one_dim_forward_benchmark()
